6a62d14ed303db4c1f7f91b7f83ac193fd0d5571
king <king@doodlemobile.com>
2016-02-23 20:45:13 +0800

    bug fix

--- src/ddb.erl
+++ src/ddb.erl
@@ -718,7 +718,7 @@ signature(SecretAccessKey, Headers, Body) ->
     StringToSign = lists:flatten(["POST", $\n, "/", $\n, $\n, canonical(Headers), $\n, Body]),
 %%    BytesToSign = crypto:sha(StringToSign),
 %%    base64:encode_to_string(binary_to_list(crypto:sha_mac(SecretAccessKey, BytesToSign))).
-    BytesToSign = crypto:hash(StringToSign, sha),
+    BytesToSign = crypto:hash(sha, StringToSign),
     base64:encode_to_string(binary_to_list(crypto:hmac(sha, SecretAccessKey, BytesToSign))).
 
 -spec canonical(proplists:proplist()) -> [_].
